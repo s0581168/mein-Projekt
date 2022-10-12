6bf2ea0e41acd0e5db59d4c8ad34119bc13e847c
thuyt <ThiThanhThuy.Nguyen@Student.HTW-Berlin.de>
2022-10-12 14:27:58 +0200

    projekt

---
+++ src/main/java/htw/webtech/demo/MeinProjektApplication.java
@@ -0,0 +1,13 @@
+package htw.webtech.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeinProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeinProjektApplication.class, args);
+	}
+
+}
